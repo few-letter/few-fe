69fdd328257b97a6c209ad7312e78877a1d7bad1
zzinLee <sujin951017@gmail.com>
2025-07-18 23:02:37 +0900

    ✨ 임시 Badge 컴포넌트

---
+++ src/shared/components/Badge.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils";
+
+const BADGE_CONFIG = {
+  TECH: {
+    color: "text-badge-purple",
+    label: "기술",
+  },
+  BUSINESS: {
+    color: "text-badge-yellow",
+    label: "비지니스",
+  },
+  APPLE: {
+    color: "text-badge-pink",
+    label: "애플",
+  },
+  ECONOMY: {
+    color: "text-badge-blue",
+    label: "경제",
+  },
+  ELECTROIC_CAR: {
+    color: "text-badge-red",
+    label: "전기차",
+  },
+  POLITICS: {
+    color: "text-badge-green",
+    label: "정치",
+  },
+} as const;
+
+export type Category = keyof typeof BADGE_CONFIG;
+
+interface BadgeProps {
+  category: Category;
+  className?: string;
+}
+
+export default function Badge({ category, className }: BadgeProps) {
+  return (
+    <div
+      className={cn(
+        `${BADGE_CONFIG[category].color} font-caption2 w-fit rounded-sm bg-white px-8 py-4`,
+        className,
+      )}
+    >
+      {BADGE_CONFIG[category].label}
+    </div>
+  );
+}
