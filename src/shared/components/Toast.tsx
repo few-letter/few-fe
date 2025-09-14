404f0c2641b20b91d981bc7266cb3182de66e8ef
zzinLee <sujin951017@gmail.com>
2025-09-14 14:35:31 +0900

    ✨ Toast추가

--- src/shared/components/Toast.tsx
+++ src/shared/components/Toast.tsx
@@ -1,10 +1,25 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import { cn } from "@/lib/utils";
+import { useState, useEffect, useRef } from "react";
+
+import type { TransitionEvent } from "react";
+
+const getToastStyles = (type: "error" | "success" | "info") => {
+  switch (type) {
+    case "error":
+      return "bg-white border-badge-red text-badge-red";
+    case "success":
+      return "bg-white border-badge-green text-badge-green";
+    case "info":
+      return "bg-white border-badge-blue text-badge-blue";
+    default:
+      return "bg-white border-gray6 text-gray6";
+  }
+};
 
 interface ToastProps {
-  message: string;
+  message: string | null;
   type?: "error" | "success" | "info";
   duration?: number;
   onClose?: () => void;
@@ -12,49 +27,46 @@ interface ToastProps {
 
 export const Toast = ({
   message,
-  type = "error",
+  type = "info",
   duration = 3000,
   onClose,
 }: ToastProps) => {
   const [isVisible, setIsVisible] = useState(false);
+  const styles = getToastStyles(type);
+  const timer = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     if (message) {
       setIsVisible(true);
 
-      const timer = setTimeout(() => {
+      timer.current = setTimeout(() => {
         setIsVisible(false);
-        setTimeout(() => onClose?.(), 300);
       }, duration);
 
-      return () => clearTimeout(timer);
+      return () => {
+        if (timer.current) {
+          clearTimeout(timer.current);
+        }
+      };
+    } else {
+      setIsVisible(false);
     }
   }, [message, duration, onClose]);
 
-  if (!message) return null;
-
-  const getToastStyles = () => {
-    switch (type) {
-      case "error":
-        return "bg-red1 border-red3 text-red3";
-      case "success":
-        return "bg-green1 border-green3 text-green3";
-      case "info":
-        return "bg-blue1 border-blue3 text-blue3";
-      default:
-        return "bg-red1 border-red3 text-red3";
-    }
+  const handleTransitionEnd = (e: TransitionEvent<HTMLDivElement>) => {
+    if (!isVisible && e.propertyName === "opacity") onClose?.();
   };
 
   return (
     <div
       className={cn(
-        "pointer-events-none fixed right-16 bottom-20 z-50",
-        "font-body6 text-14 rounded-sm border px-16 py-12",
-        "transition-all duration-300 ease-in-out",
-        getToastStyles(),
         isVisible ? "opacity-100" : "opacity-0",
+        "pointer-events-none fixed top-200 left-1/2 z-50 -translate-x-1/2",
+        "font-body6 text-14 rounded-sm border px-16 py-8 shadow-lg backdrop-blur-sm",
+        `transform-gpu transition-all duration-300 ease-[cubic-bezier(0.16,1,0.3,1)]`,
+        styles,
       )}
+      onTransitionEnd={handleTransitionEnd}
     >
       {message}
     </div>
