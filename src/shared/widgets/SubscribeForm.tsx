371ecd790b9f50ebacca942c3af48d9015845c40
zzinLee <sujin951017@gmail.com>
2025-09-14 14:00:19 +0900

    ✨ SubscriptionForm에 API추가

--- src/shared/widgets/SubscribeForm.tsx
+++ src/shared/widgets/SubscribeForm.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { Checkboxes, EmailInput } from "@/shared/components";
+import { Checkboxes, EmailInput, Toast } from "@/shared/components";
 import { useState } from "react";
 import { validateEmail } from "@/shared/utils/util";
+import { useMutation } from "@tanstack/react-query";
+import { postSubscriptionsMutation } from "@/shared/remotes/postSubscriptions";
+import {
+  SUCCESS_SUBSCRIPTION_TOAST_MESSAGE,
+  ERROR_SUBSCRIPTION_TOAST_MESSAGE,
+} from "@/shared/constants/util";
 
 import type { CategoryCode } from "@/shared/types";
 import type { CodeType } from "@/shared/components/Checkboxes";
@@ -21,20 +27,52 @@ export const SubscribeForm = ({ categories }: SubscribeFormProps) => {
     email: "",
     categoryCodes: [],
   });
+  const [successToastMessage, setSuccessToastMessage] = useState<string | null>(
+    null,
+  );
+  const [errorToastMessage, setErrorToastMessage] = useState<string | null>(
+    null,
+  );
+  const initializeForm = () => setForm({ email: "", categoryCodes: [] });
+  const subscriptionMutation = useMutation({
+    ...postSubscriptionsMutation(),
+    onSuccess: (data) => {
+      console.log("구독이 완료되었습니다:", data);
+      initializeForm();
+      setSuccessToastMessage(SUCCESS_SUBSCRIPTION_TOAST_MESSAGE);
+    },
+    onError: (error: Error) => {
+      console.error("구독 중 오류가 발생했습니다:", error.message);
+      setErrorToastMessage(ERROR_SUBSCRIPTION_TOAST_MESSAGE);
+    },
+    onMutate: (variables) => {
+      console.log("구독 요청 시작:", variables);
+    },
+  });
+
   const isDisabled =
     form.email === "" ||
     !validateEmail(form.email) ||
-    form.categoryCodes.length === 0;
+    form.categoryCodes.length === 0 ||
+    subscriptionMutation.isPending;
 
   const handleEmailChange = (value: string) => {
-    setForm({ ...form, email: value });
+    setForm({ ...form, email: value.trim() });
   };
 
   const handleCategoryCodesChange = (value: CodeType[]) => {
     setForm({ ...form, categoryCodes: value });
   };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    if (isDisabled) return;
+
+    subscriptionMutation.mutate({
+      email: form.email,
+      categoryCodes: form.categoryCodes as CategoryCode[],
+    });
   };
 
   return (
@@ -62,9 +100,21 @@ export const SubscribeForm = ({ categories }: SubscribeFormProps) => {
           type="submit"
           className="bg-gray10 font-sub6 disabled:bg-gray4 disabled:text-gray7 w-full rounded-sm py-16 text-white transition-colors"
         >
-          구독하기
+          {subscriptionMutation.isPending ? "구독 중..." : "구독하기"}
         </button>
       </form>
+      <Toast
+        message={successToastMessage}
+        type="success"
+        duration={3000}
+        onClose={() => setSuccessToastMessage(null)}
+      />
+      <Toast
+        message={errorToastMessage}
+        type="error"
+        duration={3000}
+        onClose={() => setErrorToastMessage(null)}
+      />
     </>
   );
 };
