e912e118cbfca8def07425008e929bf3d6a0e94e
zzinLee <sujin951017@gmail.com>
2025-10-06 18:07:25 +0900

    데일리 few. 4줄 이상

--- src/shared/components/NewsCard.tsx
+++ src/shared/components/NewsCard.tsx
@@ -51,7 +51,7 @@ export const NewsCard = ({
           <div className="space-y-12">
             <Badge categoryCode={categoryCode} categories={categories} />
             <div className="font-sub2 line-clamp-2 text-white">{headline}</div>
-            <p className="font-body6 text-gray4 line-clamp-3">
+            <p className="font-body6 text-gray4 line-clamp-4">
               <HighlightedText text={summary} highlightTexts={highlightTexts} />
             </p>
           </div>
