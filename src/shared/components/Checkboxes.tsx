002f7c8d13f1a42ab54430f5437cf2606b3df768
zzinLee <sujin951017@gmail.com>
2025-10-06 21:29:53 +0900

    label에러 처리

--- src/shared/components/Checkboxes.tsx
+++ src/shared/components/Checkboxes.tsx
@@ -33,47 +33,48 @@ export const Checkboxes = <T extends { code: CodeType; value: string }>({
   return (
     <div className="flex flex-col gap-12">
       {label && <label className="font-sub6 text-gray9">{label}</label>}
-
-      <fieldset className="flex flex-wrap justify-center gap-8">
-        {options.map((option) => {
-          const emoji = CATEGORY_CODE_TO_EMOJI[option.code as CategoryCode];
-          return (
-            <div
-              onClick={(e) => {
-                e.preventDefault();
-                toggleChecked(option.code);
-              }}
-              key={option.code}
-              className={cn(
-                "group cursor-pointer rounded-sm px-12 py-8 transition-colors",
-                value.includes(option.code)
-                  ? "bg-blue2 hover:bg-blue3"
-                  : "bg-gray2 hover:bg-gray3",
-              )}
-            >
-              <label htmlFor={String(option.code)} className="cursor-pointer">
-                <input
-                  type="checkbox"
-                  id={String(option.code)}
-                  name={name}
-                  value={option.code}
-                  className="appearance-none"
-                />
-                <span
-                  className={cn(
-                    "font-body6 transition-colors",
-                    value.includes(option.code)
-                      ? "font-sub6 text-gray10"
-                      : "text-gray10 group-hover:text-gray9",
-                  )}
-                >
-                  {`${emoji} ${option.value}`}
-                </span>
-              </label>
-            </div>
-          );
-        })}
-      </fieldset>
+      <div className="flex w-full justify-start">
+        <div className="flex flex-wrap justify-center gap-8">
+          {options.map((option) => {
+            const emoji = CATEGORY_CODE_TO_EMOJI[option.code as CategoryCode];
+            return (
+              <div
+                onClick={(e) => {
+                  e.preventDefault();
+                  toggleChecked(option.code);
+                }}
+                key={option.code}
+                className={cn(
+                  "group cursor-pointer rounded-sm px-12 py-8 transition-colors",
+                  value.includes(option.code)
+                    ? "bg-blue2 hover:bg-blue3"
+                    : "bg-gray2 hover:bg-gray3",
+                )}
+              >
+                <label htmlFor={String(option.code)} className="cursor-pointer">
+                  <input
+                    type="checkbox"
+                    id={String(option.code)}
+                    name={name}
+                    value={option.code}
+                    className="appearance-none"
+                  />
+                  <span
+                    className={cn(
+                      "font-body6 transition-colors",
+                      value.includes(option.code)
+                        ? "font-sub6 text-gray10"
+                        : "text-gray10 group-hover:text-gray9",
+                    )}
+                  >
+                    {`${emoji} ${option.value}`}
+                  </span>
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
