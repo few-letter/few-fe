c3cc67a1683411e7617200a7be3e47417a374bc7
zzinLee <sujin951017@gmail.com>
2025-12-28 18:16:51 +0900

    헤더 무료 구독하기 링크에 Mixpanel 이벤트 추적 추가
    - Header 컴포넌트를 client 컴포넌트로 변경
    - 무료 구독하기 링크 클릭 시 HOME_NAV_SUBSCRIBE_CLICK 이벤트 추적
    - MIXPANEL_EVENT에 HOME_NAV_SUBSCRIBE_CLICK 상수 추가

--- src/shared/constants/mixpanelTrackEvent.ts
+++ src/shared/constants/mixpanelTrackEvent.ts
@@ -2,6 +2,7 @@ export const MIXPANEL_EVENT = {
   SESSION_START: "session_start",
   HOME_MAIN_APPEAR: "home_main_appear",
   HOME_CATEGORY_CLICK: "home_category_click",
+  HOME_NAV_SUBSCRIBE_CLICK: "home_nav_subscribe_click",
 } as const;
 
 export type MixpanelEvent =
