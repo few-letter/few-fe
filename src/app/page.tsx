143bb847bb1945ec0fc683626bacb13f77327181
zzinLee <sujin951017@gmail.com>
2026-01-08 13:18:11 +0900

    useGroups, DailyFewSection의 사용처에서 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,11 @@ import { Header } from "@/shared/components";
 
 import { getGroupsOptions, getCategoriesOptions } from "@/shared/remotes";
 import { getQueryClient } from "@/api/client/queryClient";
-import {
-  formatDateToYYYYMMDD,
-  formatKoreanDate,
-  getRefreshDate,
-} from "@/shared/utils";
+import { formatKoreanDate, getRefreshDate } from "@/shared/utils";
 
 export default async function Home() {
   const queryClient = getQueryClient();
   const newsDate = getRefreshDate(new Date());
-  const newsDateFormatted = formatDateToYYYYMMDD(newsDate);
   const newsDateFormattedKorean = formatKoreanDate(newsDate);
 
   await Promise.all([
@@ -35,7 +30,7 @@ export default async function Home() {
         <section className="px-16">
           <DailyFewHeader currentDate={newsDateFormattedKorean} />
           <div className="flex w-full flex-col gap-24 overflow-hidden pb-40 md:flex-row">
-            <DailyFewSection date={newsDateFormatted} />
+            <DailyFewSection />
           </div>
         </section>
         <div className="bg-gray2 h-16 w-full lg:hidden" />
