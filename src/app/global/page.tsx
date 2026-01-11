329faab5ac808ddc5c60963b36dc2fce54ac5bd2
zzinLee <sujin951017@gmail.com>
2026-01-11 21:47:03 +0900

    global 페이지 추가

--- src/app/global/page.tsx
+++ src/app/global/page.tsx
@@ -1,21 +1,52 @@
+import { Suspense } from "react";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import {
+  DailyFewHeader,
+  DailyFewSection,
+  DailyFewSummary,
+  HomeTracker,
+} from "@/shared/widgets";
 import { Header } from "@/shared/components";
-import { getRefreshDate, formatDateToYYYYMMDD } from "@/shared/utils";
-import { DailyFewHeader } from "@/shared/widgets";
+import { getGroupsOptions, getCategoriesOptions } from "@/shared/remotes";
+import { getQueryClient } from "@/api/client/queryClient";
+import {
+  formatDateToYYYYMMDD,
+  formatKoreanDate,
+  getRefreshDate,
+} from "@/shared/utils";
+import { WorldType } from "@/shared/types";
 
 export default async function Home() {
+  const queryClient = getQueryClient();
   const newsDate = getRefreshDate(new Date());
   const newsDateFormatted = formatDateToYYYYMMDD(newsDate);
+  const newsDateFormattedKorean = formatKoreanDate(newsDate);
+
+  await Promise.all([
+    queryClient.prefetchQuery(getCategoriesOptions(WorldType.GLOBAL)),
+    queryClient.prefetchQuery(
+      getGroupsOptions(WorldType.GLOBAL, newsDateFormatted),
+    ),
+  ]);
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <HomeTracker />
       <Header />
       <main className="m-auto max-w-1200">
         <section className="px-16">
-          <DailyFewHeader currentDate={newsDateFormatted} />
-          <div className="flex w-full flex-col gap-24 overflow-hidden pb-40 md:flex-row"></div>
+          <DailyFewHeader currentDate={newsDateFormattedKorean} />
+          <div className="flex w-full flex-col gap-24 overflow-hidden pb-40 md:flex-row">
+            <DailyFewSection date={newsDateFormatted} />
+          </div>
         </section>
         <div className="bg-gray2 h-16 w-full lg:hidden" />
-        <section className="px-16"></section>
+        <section className="px-16">
+          <Suspense fallback={<div>loading...</div>}>
+            <DailyFewSummary />
+          </Suspense>
+        </section>
       </main>
-    </>
+    </HydrationBoundary>
   );
 }
