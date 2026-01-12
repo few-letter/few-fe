a29acfc365213e65248ce1fb7e58e1b541d8065a
zzinLee <sujin951017@gmail.com>
2026-01-12 17:50:49 +0900

    카테고리 별 카드 기본 이미지 추가

--- src/shared/widgets/DailyFewSection.tsx
+++ src/shared/widgets/DailyFewSection.tsx
@@ -2,6 +2,7 @@
 
 import { Carousel, NewsCard } from "@/shared/components";
 import { useGroups, usePathToWorld } from "@/shared/hooks";
+import { CATEGORY_CODE_TO_IMAGE } from "@/shared/constants";
 
 export const DailyFewSection = () => {
   const world = usePathToWorld();
@@ -17,6 +18,7 @@ export const DailyFewSection = () => {
           headline={item.headline}
           summary={item.summary}
           relatedNews={item.groupSourceHeadlines}
+          image={CATEGORY_CODE_TO_IMAGE[item.category]}
         />
       )}
       numColumns={2}
