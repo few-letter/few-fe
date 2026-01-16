d764b5d77ecc470b218b7a6f3df5170d1f76ad44
zzinLee <sujin951017@gmail.com>
2026-01-16 16:02:03 +0900

    page.tsx의 WorldType enum로 변경

---
+++ src/app/page.tsx
@@ -0,0 +1,46 @@
+import { Suspense } from "react";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import {
+  DailyFewHeader,
+  DailyFewSection,
+  DailyFewSummary,
+  HomeTracker,
+} from "@/shared/widgets";
+import { Header } from "@/shared/components";
+
+import { getGroupsOptions, getCategoriesOptions } from "@/shared/remotes";
+import { getQueryClient } from "@/api/client/queryClient";
+import { formatKoreanDate, getRefreshDate } from "@/shared/utils";
+import { WorldType } from "@/shared/types";
+
+export default async function Home() {
+  const queryClient = getQueryClient();
+  const newsDate = getRefreshDate(new Date());
+  const newsDateFormattedKorean = formatKoreanDate(newsDate);
+
+  await Promise.all([
+    queryClient.prefetchQuery(getCategoriesOptions(WorldType.LOCAL)),
+    queryClient.prefetchQuery(getGroupsOptions(WorldType.LOCAL)),
+  ]);
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <HomeTracker />
+      <Header />
+      <main className="m-auto max-w-1200">
+        <section className="px-16">
+          <DailyFewHeader currentDate={newsDateFormattedKorean} />
+          <div className="flex w-full flex-col gap-24 overflow-hidden pb-40 md:flex-row">
+            <DailyFewSection />
+          </div>
+        </section>
+        <div className="bg-gray2 h-16 w-full lg:hidden" />
+        <section className="px-16">
+          <Suspense fallback={<div>loading...</div>}>
+            <DailyFewSummary />
+          </Suspense>
+        </section>
+      </main>
+    </HydrationBoundary>
+  );
+}
